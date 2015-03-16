f185054888c60134e1012dc01478a091e270d684
Cole Geissinger <colegeissinger@gmail.com>
2015-03-15 22:16:04 -0700

    Adding wp_title filter

--- includes/functions/core.php
+++ includes/functions/core.php
@@ -94,3 +94,33 @@ function header_meta() {
 
 	echo apply_filters( 'wimp_humans', $meta );
 }
+
+/**
+ * Filters wp_title to print a neat <title> tag based on what is being viewed.
+ *
+ * @since 0.1.0
+ */
+function _wimp_wp_title( $title, $sep ) {
+	global $page, $paged;
+
+	if ( is_feed() ) {
+		return $title;
+	}
+
+	// Add the blog name
+	$title .= get_bloginfo( 'name' );
+
+	// Add the blog description for the home/front page.
+	$site_description = get_bloginfo( 'description', 'display' );
+	if ( $site_description && ( is_home() || is_front_page() ) ) {
+		$title .= " $sep $site_description";
+	}
+
+	// Add a page number if necessary:
+	if ( $paged >= 2 || $page >= 2 ) {
+		$title .= " $sep " . sprintf( __( 'Page %s', 'wimp' ), max( $paged, $page ) );
+	}
+
+	return $title;
+}
+add_filter( 'wp_title', '_wimp_wp_title', 10, 2 );
