503dc61a430b080aec79d63415cf66cc209d5db1
Cole Geissinger <colegeissinger@gmail.com>
2015-05-17 23:21:42 -0700

    Continuing work on the homepage

---
+++ front-page.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+
+	<section class="content-wrapper">
+		<aside class="top-banner">
+			<div class="banner-content">
+				<h3>WIMP is a <span>North Bay Area</span> Community<br />of web & digital media professionals.</h3>
+				<div class="featured-callout">We are industry experts and students alike with a common goal to educate, be educated and help cultivate each other’s careers.</div>
+			</div>
+		</aside>
+	</section>
+
+<?php get_footer(); ?>
